db2ea065a029e7f9c38cbbd258af90c8302a80e5
Thomas Edson <andepipous@gmail.com>
2025-10-02 02:36:11 -0400

    feat: configure product module routing

--- src/app/modules/admin/products/products.routes.ts
+++ src/app/modules/admin/products/products.routes.ts
@@ -3,14 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./containers/product-list-page/product-list-page.component').then((m) => m.ProductListPageComponent)
+        loadComponent: () => import('./containers/product-list-page/product-list-page.component').then((m) => m.ProductListPageComponent),
+        data: {
+            title: 'Produtos',
+            description: 'Listagem de produtos'
+        }
     },
     {
         path: 'create',
-        loadComponent: () => import('./containers/product-create-edit/product-create-edit.component').then((m) => m.ProductCreateEditComponent)
+        loadComponent: () => import('./containers/product-create/product-create.component').then((m) => m.ProductCreateComponent),
+        data: {
+            title: 'Produtos',
+            description: 'Criar um novo produto'
+        }
     },
     {
         path: 'edit/:id',
-        loadComponent: () => import('./containers/product-create-edit/product-create-edit.component').then((m) => m.ProductCreateEditComponent)
+        loadComponent: () => import('./containers/product-edit/product-edit.component').then((m) => m.ProductEditComponent)
+        
     }
 ]
