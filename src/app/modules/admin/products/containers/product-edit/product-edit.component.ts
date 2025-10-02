6348f7b1788a48fcb2cf24622bb9ee5f17f42220
Thomas Edson <andepipous@gmail.com>
2025-10-02 02:41:55 -0400

    feat: add product edit container component

---
+++ src/app/modules/admin/products/containers/product-edit/product-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-edit',
+  imports: [
+    
+  ],
+  templateUrl: './product-edit.component.html',
+  styleUrl: './product-edit.component.scss'
+})
+export class ProductEditComponent {
+  
+}
